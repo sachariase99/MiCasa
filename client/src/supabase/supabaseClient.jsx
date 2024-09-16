bad8d1498ef781b9fb32964240ced415924b45f8
sachariase99 <sachariase99@hotmail.com>
2024-09-16 18:09:04 +0200

    Update supabaseClient.jsx

--- client/src/supabase/supabaseClient.jsx
+++ client/src/supabase/supabaseClient.jsx
@@ -2,8 +2,9 @@ import { createClient } from "@supabase/supabase-js";
 import { createContext, useContext } from "react";
 
 // Initialize Supabase client once
-const supabaseUrl = 'https://xolijibfaoscceamabvd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvbGlqaWJmYW9zY2NlYW1hYnZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjMwMjk1NjAsImV4cCI6MjAzODYwNTU2MH0.1XS-HXuckL3WvyH-F4abo9omSHWcOGy00OMjnN-x800';
+const supabaseUrl = import.meta.env.VITE_API_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const SupabaseContext = createContext();
