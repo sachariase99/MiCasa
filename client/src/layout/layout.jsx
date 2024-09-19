44b8be2618ce94f493f2d74599859d871dd042d6
sachariase99 <sachariase99@hotmail.com>
2024-09-20 00:05:48 +0200

    Update layout.jsx
    Added page titles

--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -13,21 +13,36 @@ const Layout = () => {
     }
   }, [location, navigate]);
 
+  useEffect(() => {
+    const setTitleForRoute = (pathname) => {
+      const titles = {
+        "/home": "Home - MiCasa",
+        "/about": "About Us - MiCasa",
+        "/contact": "Contact - MiCasa",
+        "/estates": "Estates - MiCasa",
+        "/search": "Search Results - MiCasa",
+      };
+
+      if (pathname.startsWith("/estates/")) {
+        document.title = "Estate Details - MiCasa";
+      } else {
+        document.title = titles[pathname] || "MiCasa";
+      }
+    };
+
+    setTitleForRoute(location.pathname);
+  }, [location.pathname]);
+
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Sticky navbar */}
       <header className="fixed top-0 left-0 w-full z-10">
         <Nav />
       </header>
 
-      {/* Main content area */}
       <main className="flex-grow pt-16">
-        {" "}
-        {/* Adjust padding-top as needed */}
         <Outlet />
       </main>
 
-      {/* Footer at the bottom */}
       <footer className="mt-auto">
         <Footer />
       </footer>
